e324551146120cd379bb1eb61033e8d1f0e7204f
Youngjun Cha <youngjun73.cha@gmail.com>
2023-01-23 02:54:51 +0800

    First Commit

---
+++ test.py
@@ -0,0 +1,22 @@
+from pdfminer.high_level import extract_pages, extract_text
+import re
+import pandas as pd
+import glob
+
+pattern1 = re.compile(r'[a-zA-Z]+\s{1}')
+pattern2 = re.compile(r'[a-zA-Z]+^[a-zA-Z0-9]{1}')
+df = pd.DataFrame(columns = ['freq'])
+
+for fn in glob.glob(r'.\pdfs\*.pdf'):
+    print(fn)
+    t = extract_text(fn)
+    text = pattern1.findall(t.lower()) + pattern2.findall(t.lower())
+    for i in text:
+        i = i.replace(' ','').replace('\n','').replace('.','').replace(',','').replace('?','').replace('!','')
+        if not i in list(df.index):
+            df.loc[i,'freq']=1
+        else:
+            df.loc[df.index==i,'freq'] += 1
+
+df = df.sort_values(by='freq', ascending=False)
+df.to_csv('sat_word_freq.csv')

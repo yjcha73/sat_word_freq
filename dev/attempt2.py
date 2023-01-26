1878bba9794f502dd7a39e8326d497e33a6de81c
ACE-OF-DIAMONDS <53719487+mystery-z@users.noreply.github.com>
2023-01-27 01:23:39 +0800

    +gitignore

--- dev/attempt2.py
+++ dev/attempt2.py
@@ -4,33 +4,36 @@ import json
 # ~ requires installation of tesseract for ocr
 # ~ windows: https://github.com/tesseract-ocr/tesseract#installing-tesseract
 # ~ mac: brew install Tesseract
+# ~ please ensure that the version is tesseract 5.3.0
+
 from PIL import Image
 import pytesseract
+import os
 import numpy as np
 
-
-# ~ -----essentially doing pdf --> jpeg conversion
 pdfs = r"/Users/juwoncha/Documents/GitHub/sat_word_freq/pdfs/2021 March SAT QAS.pdf"
-# ~ TODO fix the pdf file path ^^^ before running 
-pages = convert_from_path(pdfs, 350)
 
-i = 1
-
-all_text = ""
-
-# ~ turn every page in the pdf into a jpeg file
-for page in pages:
-	image_name = "Page_" + str(i) + ".jpg"  
-	page.save(image_name, "JPEG")
-	i = i+1        
-    # ~ -----doing the ocr for the jpegs-------
-	img1 = np.array(Image.open(image_name))
-	text = pytesseract.image_to_string(img1)
-	all_text = all_text.__add__(text)
-	print("done "+image_name)
-	
-with open('data.txt','w') as file:
-	file.write(all_text)
+def get_full_text(pdfs):
+	# ~ -----essentially doing pdf --> jpeg conversion
+	# ~ TODO fix the pdf file path ^^^ before running 
+	pages = convert_from_path(pdfs, 350)
+	all_text = ""
+	i = 1
+	# ~ turn every page in the pdf into a jpeg file
+	for page in pages:
+		image_name = "Page_" + str(i) + ".jpg"  
+		page.save(image_name, "JPEG")
+		# ~ -----doing the ocr for the jpegs-------
+		img1 = np.array(Image.open(image_name))
+		text = pytesseract.image_to_string(img1)
+		all_text = all_text.__add__(text)
+		print("done "+image_name)
+		i = i+1        
+		os.remove(image_name)
+	return all_text
+		
+with open('data.txt','w', encoding='utf-8') as file:
+	file.write(get_full_text(pdfs))
 	file.close()
 	
 # ~ TODO make upper and lower boundaries for the images so that the really weird margin areas are cut
